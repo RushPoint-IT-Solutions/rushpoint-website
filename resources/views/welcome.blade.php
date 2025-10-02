8893275723203219206d1ba18d3252c947fc093a
Renz Christian Cabato <renzcabato@Renzs-Mac-mini.local>
2025-10-02 22:23:08 +0800

    Done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1002,10 +1002,7 @@
                     </div>
     
                     <div class="col-12 text-center">
-                      <div class="loading">Loading</div>
-                      <div class="error-message"></div>
-                      <div class="sent-message">Your message has been sent. Thank you!</div>
-    
+                    
                       <button type="submit" class="btn btn-submit w-100">Send Message</button>
                     </div>
     
@@ -1094,7 +1091,7 @@
 
   <!-- Vendor JS Files -->
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
+  {{-- <script src="assets/vendor/php-email-form/validate.js"></script> --}}
   <script src="assets/vendor/aos/aos.js"></script>
   <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
   <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
