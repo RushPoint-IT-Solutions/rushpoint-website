762fa16ad9d64dd8fdc0b0e18c8551d08ea3f5db
Renz Christian Cabato <renzcabato@Renzs-Mac-mini.local>
2025-10-05 13:50:35 +0800

    Done

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+use Spatie\Sitemap\SitemapGenerator;
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::post('/contact', 'ContactController@send')->name('contact.send');
+Route::get('/generate-sitemap', function () {
+    SitemapGenerator::create('https://rushpoint.com.ph')->writeToFile(public_path('sitemap.xml'));
+    return 'Sitemap generated!';
+});
